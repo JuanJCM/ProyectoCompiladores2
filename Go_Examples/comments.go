b6f296650705e92655c9b0137d68410a079b60b9
Victor Vasquez <victor.vasquez96@unitec.edu>
2021-10-31 21:51:37 -0600

    Added Go examples for testing

---
+++ Go_Examples/comments.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		This
+		is
+		a
+		block
+		comment
+		var a = 5
+		false
+		Printl
+	*/
+    fmt.Println("hello world")
+	// This is a line comment
+}

e1fad20cfcca57c0ef9a16f6cfaebdc68ba73c81
JuanJCM <j.carranza@unitec.edu>
2021-11-30 00:04:40 -0600

    fixed examples

--- Go_Examples/values.go
+++ Go_Examples/values.go
@@ -4,12 +4,13 @@ import "fmt"
 
 func main() {
 
-    fmt.Println("go" + "lang")
-    fmt.Println('go2' + 'lang2')
+    //fmt.Println("go" + "lang")
+    //fmt.Println('go2' + 'lang2')
+    fmt.Println("Hello", "World")
     fmt.Println("1+1 =", 1+1)
     fmt.Println("7.0/3.0 =", 7.0/3.0)
 
-    fmt.Println(true && false)
-    fmt.Println(true || false)
-    fmt.Println(!true)
+     fmt.Println(true && false)
+     fmt.Println(true || false)
+     fmt.Println(!true)
 }
